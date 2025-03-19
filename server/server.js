b4f322a1b95fe1398983e074cffab6c9192b0703
Demi-O <demiladeolagoke@yahoo.ca>
2025-03-19 17:36:38 -0400

    added auth check to subscriptions

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import { readFile } from 'node:fs/promises';
 import { createServer as createHttpServer } from 'node:http';
 import { WebSocketServer } from 'ws';
 
-import { authMiddleware, handleLogin } from './auth.js';
+import { authMiddleware, decodeToken, handleLogin } from './auth.js';
 import { resolvers } from './resolvers.js';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
@@ -18,10 +18,21 @@ app.use(cors(), express.json());
 
 app.post('/login', handleLogin);
 
-function getContext({ req }) {
+function getHttpContext({ req }) {
   if (req.auth) {
     return { user: req.auth.sub };
   }
+  
+  return {};
+}
+
+function getWsContext({ connectionParams }) {
+  const accessToken = connectionParams?.accessToken;
+  if (accessToken ) {
+    const payload = decodeToken(accessToken)
+    return { user: payload.sub };
+  }
+
   return {};
 }
 
@@ -31,12 +42,12 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 const apolloServer = new ApolloServer({ schema });
 await apolloServer.start();
 app.use('/graphql', authMiddleware, apolloMiddleware(apolloServer, {
-  context: getContext,
+  context: getHttpContext,
 }));
 
 const httpServer = createHttpServer(app);
 const wsServer = new WebSocketServer({ server: httpServer, path: '/graphql' });
-UseWsServer({ schema }, wsServer);
+UseWsServer({ schema, context: getWsContext }, wsServer);
 
 httpServer.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
